35961f3c50987bdc5fb053627ddaa120550c833c
Papu Arza <papuarza@MacBook-Pro-de-Papu.local>
2017-02-13 17:40:22 +0100

    done till iteration 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,25 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+const ensureLogin = require("connect-ensure-login");
+const flash = require("connect-flash");
 
-const app = express();
 
+// var index = require('./routes/index');
+// var ussers = require('./routes/siteController');
 // Controllers
 const siteController = require("./routes/siteController");
 
-// Mongoose configuration
-mongoose.connect("mongodb://localhost/ibi-ironhack");
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,14 +27,64 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('combined'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure express session middleware
+app.use(session({
+  secret: 'passport-local-strategy',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Set passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Set passport strategy
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: 'Incorrect username' });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: 'Incorrect password' });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// Set passport serializeUser
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+// Set passport deserializeUser
+passport.deserializeUser((id, cb) => {
+  User.findOne({ '_id': id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+// Mongoose configuration
+mongoose.connect("mongodb://localhost/ibi-ironhack");
+
 // Routes
-app.use("/", siteController);
+app.use('/', siteController);
+// app.use('/courses', courses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
