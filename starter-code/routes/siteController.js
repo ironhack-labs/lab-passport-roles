79039704f9c39c84f7a45c7d8c8b523b57507779
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-14 11:47:43 +0100

    iteration 2 done.

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -13,16 +13,48 @@ const checkDeveloper = checkRoles('Developer');
 const checkTA = checkRoles('TA');
 
 siteController.get("/", ensureAuthenticated, (req, res, next) => {
-    if (req.user.role === 'Boss'){
-      return res.render('index', {
-        isBoss: true
-      });
-    }
-    res.render("index");
+    User.find({}, (err, docs) => {
+        if (err) return next(err);
+        let renderObj = {isBoss: false, userId: req.user.id, users: docs};
+        if (req.user.role === 'Boss') {
+            renderObj.isBoss = true;
+        }
+        res.render("index", renderObj);
+    });
+});
+
+siteController.get("/profile/:id", ensureAuthenticated, (req, res, next) => {
+    User.findById(req.params.id, (err, doc) => {
+        if (err) return next(err);
+        if (!doc) return res.redirect('/');
+        res.render('show', {
+            user: doc
+        });
+    });
+
+});
+
+siteController.get("/profile/edit/:id", ensureIsId, (req, res, next) => {
+    res.render('edit', {
+        user: req.user
+    });
+});
+
+siteController.post("/profile/edit/:id", ensureIsId, (req, res, next) => {
+    const {username, name, familyName} = req.body;
+    User.findByIdAndUpdate(req.params.id, {username, name, familyName}, (err, doc) => {
+      if (err) return next(err);
+      res.redirect(`/profile/${req.params.id}`);
+    });
 });
 
 siteController.get("/login", (req, res, next) => {
     res.render('login');
+});
+
+siteController.get("/logout", (req, res, next) => {
+    req.logout();
+    res.redirect("/login");
 });
 
 siteController.post("/login",
@@ -78,11 +110,22 @@ siteController.post("/admin", checkBoss, (req, res, next) => {
             password: hash
         });
         newUser.save((err, user) => {
-          if (err) return next(err);
-          return res.redirect('/admin');
+            if (err) return next(err);
+            return res.redirect('/admin');
         });
     });
 });
+
+function ensureIsId(req, res, next) {
+    if (req.isAuthenticated()) {
+        if (req.params.id === req.user.id) {
+            return next();
+        }
+        return res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+}
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
