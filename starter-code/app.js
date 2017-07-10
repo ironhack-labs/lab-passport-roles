75615d54cbcd21c8cc6235516bddea3d1c99b7a4
Daniele <daniele.germani.85@gmail.com>
2017-07-10 22:31:02 +0200

    iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,11 +6,12 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const mongoose     = require("mongoose");
 
+
 const app = express();
 
 // Controllers
 const siteController = require("./routes/siteController");
-
+const signup = require("./routes/signup");
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
 
@@ -27,7 +28,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
+
 app.use("/", siteController);
+app.use("/", signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const siteController = express.Router();
+//const ensureLogin = require("connnect-ensure-login");
+
+// siteController.use((req,res)=>{
+//   if (req.user && req.user.role === "Boss") {
+//     next();
+//
+//   }else if(!req.user){
+//     res.status(403).send("User is no logged user!");
+//
+//   }else {
+//     res.status(403).send("you are not persmissions here!!");
+//   }
+// });
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
