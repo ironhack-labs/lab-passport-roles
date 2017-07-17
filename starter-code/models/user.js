48273cfe258a970e07230a9b5ae5de2aef3ee3c7
polgasull <pol89.gn@gmail.com>
2017-07-17 23:11:37 +0200

    signup

---
+++ starter-code/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  name: String,
+  familyName: String,
+  password: String,
+  role: {
+    type: String,
+    enum : ['Boss', 'Developer', 'TA'],
+    default : 'Developer'
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,45 @@
 const express = require("express");
 const siteController = express.Router();
 
+const User = require('../models/user');
+
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+
 siteController.get("/", (req, res, next) => {
   res.render("index");
+});
+
+siteController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+siteController.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 });
 
 module.exports = siteController;
