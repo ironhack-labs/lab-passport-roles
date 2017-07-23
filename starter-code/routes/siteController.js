4aa5de89f29c000a7b8bdd3bbbb94053d604e60d
polgasull <pol89.gn@gmail.com>
2017-07-23 12:00:20 +0200

    courses list ejs

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
 
 const User = require('../models/user');
+const Course = require('../models/course');
 
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
@@ -63,5 +64,16 @@ siteController.get("/logout", (req, res) => {
 siteController.get('/bossPage', ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render('bossPage', {user: req.user});
 });
+
+siteController.get('/courses', (req, res, next) => {
+  Course.find({}, (err, courses) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('courses/courses', { courses });
+    }
+  })
+})
+
 
 module.exports = siteController;
