4bcc41668385fd58c91212eb657370c6ef8b5b61
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-09-11 21:19:02 +0200

    working on iteration2

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -11,52 +11,30 @@ siteController.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//user profile
 siteController.get('/private', ensureAuthenticated, (req, res) => {
-  res.render('private', {user: req.user, name:req.user.name, username:req.user.username});
+  res.render('private', {user: req.user, name:req.user.name, username:req.user.username, role:req.user.role,
+  password:req.user.password});
 });
 
-siteController.get("/signup", (req, res) => {
-  res.render("passport/signup")
-})
+//user update itself
+siteController.get("/edit", (req, res, next) => {
+  res.render("edit", {user: req.user});
+  });
 
-siteController.get("/signup", (req, res) => {
-  res.render("passport/signup")
-})
+siteController.post('/edit', (req, res, next) => {
+  const userId = req.params.id;
 
-
-siteController.post("/signup", (req, res, next) => {
-  const username = req.body.username
-  const password = req.body.password
-  const role = req.body.role
-
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" })
-    return
-  }
-
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("passport/signup", { message: "The username already exists" })
-      return
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt)
-    const hashPass = bcrypt.hashSync(password, salt)
-
-    debug("User created")
-
-    const newUser = new User({
-      username,
-      password: hashPass,
-      role
-    })
-    .save()
-    .then(user => res.redirect('/'))
-    .catch(e => res.render("auth/signup", { message: "Something went wrong" }))
-
-  })
-})
-
+  const updates = {
+        username: req.body.username,
+        password: req.body.password,
+        role: req.body.role
+  };
+  User.findByIdAndUpdate(userId, updates, (err, user) => {
+    if (err){ return next(err); }
+    return res.redirect('/private');
+  });
+});
 
 //login
 
@@ -121,6 +99,51 @@ User.findByIdAndRemove(userId, (err, user) => {
   return res.redirect('/list');
 });
 });
+
+//create user
+siteController.get("/create", (req, res) => {
+  res.render("passport/create")
+})
+
+siteController.get("/create", (req, res) => {
+  res.render("passport/create")
+})
+
+
+siteController.post("/create", (req, res, next) => {
+  const username = req.body.username
+  const password = req.body.password
+  const role = req.body.role
+
+  if (username === "" || password === "") {
+    res.render("auth/create", { message: "Indicate username and password" })
+    return
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/create", { message: "The username already exists" })
+      return
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(password, salt)
+
+    debug("User created")
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+      role
+    })
+    .save()
+    .then(user => res.redirect('/'))
+    .catch(e => res.render("passport/create", { message: "Something went wrong" }))
+
+  })
+})
+
+
 
 
 
