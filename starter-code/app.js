e7f9e7ce86a2db44b16004f7c2b103946f7b9d08
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-11 21:28:54 +0200

    Iteration #1: Give access to General Manager DONE

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,50 +1,77 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const express        = require('express')
+const path           = require('path')
+const favicon        = require('serve-favicon')
+const logger         = require('morgan')
+const cookieParser   = require('cookie-parser')
+const bodyParser     = require('body-parser')
+const mongoose       = require("mongoose")
+const session        = require("express-session")
+const passport       = require("passport")
+const flash          = require("connect-flash")
+const expressLayouts = require('express-ejs-layouts')
+const MongoStore     = require("connect-mongo")(session)
 
-const app = express();
+const app = express()
 
 // Controllers
-const siteController = require("./routes/siteController");
+const siteController = require("./routes/siteController")
+const authController = require("./routes/auth")
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/ibi-ironhack");
+mongoose.connect("mongodb://localhost/ibi-ironhack")
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('layout', 'index')
+app.use(expressLayouts)
+
+app.use(flash())
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}))
+
+require('./passport/serializers')
+require('./passport/local')
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('combined'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
-app.use("/", siteController);
+app.use("/", siteController)
+app.use("/", authController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
