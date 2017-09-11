c7012306fe9a104e9e13f518d8584a88c3cd6c75
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-11 20:03:14 +0200

    el jefe crea usuarios

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -31,10 +31,64 @@ siteController.get('/private', checkBoss, (req, res) => {
   User.find({}).exec(function(err, users) {
     if (err) throw err;
     res.render('private', {
+      user: req.user,
       users: users
     });
   });
 });
+
+
+// siteController.get('/edit', (req, res) => {
+//   res.render('');
+// });
+
+siteController.get('/create', (req, res) => {
+  res.render('create');
+});
+
+siteController.post('/create', (req, res) => {
+const username = req.body.username;
+const password = req.body.password;
+const name = req.body.name;
+const familyName = req.body.familyName;
+const role = req.body.role;
+
+
+if (username === "" || password === "" || name === "" || familyName === "" || role === "") {
+  res.render("/create", {
+    errorMessage: "All fields requiered"
+  });
+  return;
+}
+
+User.findOne({ "username": username }).then(user =>{
+  if(user){
+    res.render("/create", {
+      errorMessage: "User already exists"
+    });
+    return;
+  }
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+  new User({
+      username: username,
+      name: name,
+      familyName: familyName,
+      password: hashPass,
+      role: role
+
+    })
+    .save()
+    .then(() => res.redirect('/private'))
+    .catch(e => next(e));
+});
+
+});
+
+// siteController.get('/delete', (req, res) => {
+//   res.render('');
+// });
+//
 
 
 siteController.get("/logout", (req, res, next) => {
