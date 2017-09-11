49dad0388d65ff76762eb89405b5716c4b4989d4
ByronGBP <byrongbp@gmail.com>
2017-09-11 23:39:10 +0200

    done editProfile with few bugs

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -61,6 +61,66 @@ siteController.post("/newEmployee", (req, res, next) => {
   });
 });
 
+siteController.get("/editProfile", ensureLogin.ensureLoggedIn("/"),(req, res, next) => {
+  res.render("editProfile", {user: req.user, roles: roles});
+});
+
+//TODO: - DRY!!
+siteController.post("/editProfile", ensureLogin.ensureLoggedIn("/"),(req, res, next) => {
+  const username = req.body.username;
+  const oldUsername = req.user.username;
+  const password = req.body.password;
+  const name = req.body.name;
+  const familyName = req.body.familyName;
+  const role = req.body.role;
+  const id = req.user._id;
+
+  let modified = {};
+
+  if (username === "") {
+    res.render("editProfile", { user: req.user, roles: roles, message: "Username can't be empty" });
+    return;
+  }
+
+
+//TODO: - Is asynchronus.
+//TODO: Think how to check if want to change the username and dat username isnt in the db already
+  // if (username !== oldUsername) {
+  //   User.findOne({ username }, "username", (err, user) => {
+  //
+  //     if (user !== null) {
+  //       console.log("--------------Find it!!!");
+  //       res.render("editProfile", {user: req.user, roles: roles, message: "The username already exists" });
+  //       return;
+  //     }
+  //   });
+  // }
+
+  if(password){
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    modified.password = hashPass;
+  }
+  if (name){
+    modified.name = name;
+  }
+  if(familyName) {
+    modified.familyName = familyName;
+  }
+  if (role) {
+    modified.role = role;
+  }
+
+  User.findByIdAndUpdate(id, modified, (err, user) => {
+    if (err) {
+      res.render("editProfile", {user: user, roles: roles, message: "Something wrong" });
+      return;
+    }
+    //TODO:- Check why dont change the fields when we are rendering again
+    res.render("editProfile", {user: user, roles: roles, message: "Edited" });
+  });
+});
+
 function checkRoles(role) {
   return function(req, res, next) {
     if (req.isAuthenticated() && req.user.role === role) {
