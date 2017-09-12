2f1e4b0c55ca05e1ff66d1b7a50dd9734985e85a
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-12 10:45:34 +0200

    Iteration 3 - updating courses

--- starter-code/routes/TAController.js
+++ starter-code/routes/TAController.js
@@ -17,11 +17,34 @@ const checkTA = checkRoles('TA');
 router.get("/", checkTA, (req, res, next) => {
   Course.find({})
   .then( response => {
-    console.log('cursos:',response)
     res.render('courses/index', { courses: response })
   }).catch( err => { next(err) } )
 });
 
+//UPDATE
+router.get("/:id/edit", checkTA, (req, res, next) => {
+  const courseID = req.params.id
+  Course.findById( courseID )
+  .then( response => {
+    res.render('courses/edit', {courseInfo: response})
+  }).catch( err => {next(err)})
+});
+
+router.post('/:id/edit', (req, res, next) => {
+  const courseId = req.params.id;
+
+  const updates = {
+        name: req.body.name,
+        startingDate: req.body.startingDate,
+        endDate: req.body.endDate,
+        level: req.body.level,
+        available: req.body.available
+  };
+  Course.findByIdAndUpdate(courseId, updates, (err, product) => {
+    if (err){ return next(err); }
+    return res.redirect('/courses');
+  });
+});
 
 
 module.exports = router;
