3a3e4a1d2ab5e7e78f78301b2627799201e8b0be
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-11-13 21:31:57 +0100

    2ºtask of iteration 1 finish

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,12 +1,17 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
+
+//crear
+// const index = require('./routes/index');
+// const users = require('./routes/users');
+
 
 // Controllers
 const siteController = require("./routes/siteController");
@@ -14,9 +19,53 @@ const siteController = require("./routes/siteController");
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
 
+const User = require("./models/user");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session( {
+  secret: 'our-passport-local',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+passport.serializeUser((user, cb) => {
+   cb(null, user._id);
+ });
+
+ passport.deserializeUser((id, cb) => {
+     User.findOne({ "_id": id }, (err, user) => {
+       if (err) { return cb(err); }
+       cb(null, user);
+     });
+   });
+   
+   passport.use(new LocalStrategy((username, password, next) => {
+     User.findOne({ username }, (err, user) => {
+       if (err) {
+         return next(err);
+       }
+       if (!user) {
+         return next(null, false, { message: "Incorrect username" });
+       }
+       if (!bcrypt.compareSync(password, user.password)) {
+         return next(null, false, { message: "Incorrect password" });
+       }
+
+       return next(null, user);
+         });
+       }));
+
+       app.use(passport.initialize());
+       app.use(passport.session());
+       app.use(flash());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,8 +76,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use("/", siteController);
 
+app.use('/', siteController);
+
+
+
+
+       
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
