31bb1a69cc33acca949bd8ca1f96156145ac5509
acampos91 <acampos91@hotmail.com>
2017-11-13 21:22:48 +0100

    primera iteración

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const siteController = express.Router();
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const ensureLogin = require("connect-ensure-login");
+const User = require("../models/user")
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
+});
+
+siteController.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+siteController.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+siteController.get("/private-page", (req, res) => {
+  res.render("auth/private", { user: req.user });
 });
 
 module.exports = siteController;
