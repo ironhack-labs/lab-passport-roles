080fa723941937513bcb7fd682ff66606da4985a
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-11-13 22:56:23 +0100

    First&Second Iteration Done - In progress

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,66 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const User = require('./models/user');
+const flash = require("connect-flash");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const ensureLogin = require("connect-ensure-login");
+
 
 const app = express();
 
 // Controllers
 const siteController = require("./routes/siteController");
+const adminController = require('./routes/adminController');
+const profilesController = require('./routes/profilesController');
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
+
+app.use(session({
+  secret: "passport-roles",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+//initialize passport and session here
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +73,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
 // Routes
 app.use("/", siteController);
+app.use("/profile", ensureLogin.ensureLoggedIn(), profilesController);
+app.use('/', ensureLogin.ensureLoggedIn(), adminController);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
