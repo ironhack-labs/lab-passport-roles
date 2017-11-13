12d3a003b6e37e6661cb689f0f7e0b1ec71c4f96
Tess <tessdegregorio@gmail.com>
2017-11-13 20:08:54 +0100

    Basic Login, Signup working perfectly

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,13 +5,15 @@ const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const mongoose     = require("mongoose");
-
+const User = require("./models/user");
 const app = express();
-
-// Controllers
 const siteController = require("./routes/siteController");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
 
-// Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
 
 // view engine setup
@@ -29,7 +31,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use("/", siteController);
 
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
