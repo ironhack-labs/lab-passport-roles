cb34ee0036b7932e1e1ef765624b7f4fc3a9f6a1
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-11-13 22:42:22 +0100

    the base working

---
+++ starter-code/models/course.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const courseSchema = new Schema({
+  name: String,
+  startingDate: Object,
+  endDate: Object,
+  level: String,
+  available: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
