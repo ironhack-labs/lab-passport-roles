3a66342491c946518ec64bfc2e3c2c76a92612a5
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-13 20:34:35 +0100

    first commit

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,43 @@
-const express = require("express");
-const siteController = express.Router();
+const router = require("express").Router()
+const User = require("../models/User")
+const passport = require('passport')
 
-siteController.get("/", (req, res, next) => {
-  res.render("index");
+
+
+router.get("/", (req, res, next) => {
+  res.render("index")
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', { message: req.flash("error") })
+})
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}))
+
+router.post('/logout',(req,res) =>{
+  req.logout()
+  res.redirect("/")
+})
+
+
+router.get('/private', checkRoles('Boss'), (req, res) => {
+  res.render('private', {user: req.user});
 });
 
-module.exports = siteController;
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
+
+module.exports = router
