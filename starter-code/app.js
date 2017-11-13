5d8ac64a3e1cf44b99ddede6d2acc7b86c06dff1
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-13 20:57:29 +0100

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,74 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose  = require("mongoose");
+
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
 
 const app = express();
 
 // Controllers
 const siteController = require("./routes/siteController");
+//require models
+const User = require("./models/User");
+const Course = require("./models/Course");
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
+
+
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+
+passport.use(new LocalStrategy({passReqToCallback: true},(req,username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+//enable sessions here
+
+
+app.use(session({
+  secret: "passport-local",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+//initialize passport and session here
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,73 @@
 const express = require("express");
 const siteController = express.Router();
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+// User model
+const User = require("../models/User");
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
 });
 
+siteController.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+//login///////////////////////
+siteController.get("/login", (req, res, next) => {
+  res.render("passport/login", { user: req.user });
+});
+
+siteController.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+//signup///////////////////////////
+siteController.get("/signup",ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/signup", { title: "signup",user: req.user });
+});
+
+siteController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+siteController.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 module.exports = siteController;
