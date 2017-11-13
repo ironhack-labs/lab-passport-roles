a956973938f16879d4d1c385e1064db3a60fba95
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-13 20:47:45 +0100

    model create

---
+++ starter-code/model/user.js
@@ -0,0 +1,22 @@
+// models/user.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    role: {
+    type: String,
+    enum : ['BOSS', "Developer", 'TA'],
+    default : 'GUEST'
+  },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,78 @@
 const express = require("express");
 const siteController = express.Router();
+const User = require("../model/user");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
+});
+
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+siteController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+siteController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save(err => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+siteController.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+siteController.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+//     siteController.get("/login", (req, res, next) => {
+//   res.render("auth/login", { "message": req.flash("error") });
+// }),
+    passReqToCallback: true
+  })
+);
+
+siteController.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+siteController.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = siteController;
