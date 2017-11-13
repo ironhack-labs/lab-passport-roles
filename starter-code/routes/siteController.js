759bc16f726893a44968230e55b49459e901ebf7
FranRom <maildefranz@gmail.com>
2017-11-13 16:13:16 +0100

    Authentication with passport

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,80 @@
 const express = require("express");
 const siteController = express.Router();
+// User model
+const User           = require("../models/user");
+// Bcrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+const ensureLogin = require("connect-ensure-login");
+const passport      = require("passport");
+
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
 });
+
+siteController.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+//add new route with path signup and pointing to the view
+siteController.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
+});
+
+//Post route to receive the date from the signup form and save the user
+siteController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(req.body);
+  if (username === "" || password === "") {
+    res.render("passport/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", {
+          errorMessage: "Something went wrong when signing up"
+        });
+      } else {
+         res.redirect("/login");
+      }
+    });
+  });
+});
+
+
+//LOGIN
+siteController.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+
+siteController.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
 
 module.exports = siteController;
