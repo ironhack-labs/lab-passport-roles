12d48672c3a4f01fff0c3841d32aed9a31c79123
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-14 11:49:37 +0100

    a ver ahora

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -2,21 +2,21 @@
 const mongoose = require('mongoose');
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
-const User = require('../models/user');
-const Course = require('../models/course');
+const User = require('../models/User');
+const Course = require('../models/Course');
 
 mongoose.connect("mongodb://localhost/ibi-ironhack");
 var salt = bcrypt.genSaltSync(bcryptSalt);
 const password = "ironhack";
 var encryptedPass = bcrypt.hashSync(password, salt);
 
-const boss = new User({
-  username: 'theboss',
-  name: 'Gonzalo',
-  familyName: 'M.',
-  password: encryptedPass,
-  role: 'Boss'
-});
+// const boss = new User({
+//   username: 'theboss',
+//   name: 'Gonzalo',
+//   familyName: 'M.',
+//   password: encryptedPass,
+//   role: 'Boss'
+// });
 const courses = [
   {
     name: 'Introduction to Ruby on Rails',
@@ -65,9 +65,9 @@ User.create(boss, (err, user) => {
 });
 
 Course.create(courses, (err, docs)=>{
-  if (err) { throw err };
+  if (err) { throw err; };
     docs.forEach( (course) => {
-      console.log(course.name)
-    })
+      console.log(course.name);
+    });
     mongoose.connection.close();
 });
