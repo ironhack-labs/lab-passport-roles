ed3c3938bc229085e33ef8bf38a548481d5a443f
mouyaq <mouyaq@gmail.com>
2018-01-28 02:00:45 +0100

    feat: basic files configuration

---
+++ starter-code/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    role: {
+        type: String,
+        enum: ["Boss", "Developer", "TA"]
+    }
+}, { timestamps: true} );
+
+userSchema.pre('save', function(next) {
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+        .then(salt => {
+            bcrypt.hash(this.password, salt)
+                .then(hash => {
+                    this.password = hash;
+                    next();
+                })
+        })
+        .catch(error => next(error));
+});
---
+++ starter-code/routes/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authController = require('../controllers/auth.controller');
+
+router.get("/", authController.index);
+
+module.exports = router;
