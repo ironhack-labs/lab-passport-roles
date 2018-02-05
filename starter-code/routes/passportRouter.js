e6a2b0085acd046cb956c45c571a3e67c04be8ce
Kathia Rieser <kath.medulla@gmail.com>
2018-02-05 20:09:50 +0100

    iteración 1

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -89,7 +89,43 @@ var checkDeveloper = checkRoles('Developer');
 var checkTA  = checkRoles('TA');
 
 router.get('/private', checkBoss, (req, res) => {
-  res.render('passport/private', {user: req.user});
+  //res.render('passport/private', {user: req.user});
+  User.find().exec((err, users) => {
+    res.render('passport/private', {
+      user: req.user,
+      users: users
+    });
+  });
+
+});
+
+/* CRUD -> CREATE FORM */
+router.get('/private/new', (req, res) => {
+  res.render('users/new');
+});
+
+/*CRUD -> CREATE DATABASE */
+router.post('/private/new', (req, res) => {
+  console.log("dentro del post new");
+  console.log(req.body);
+  const {username,name,familyName,password,role} = req.body;
+  const user = new User({username,name,familyName,password,role});
+  console.log("antes de crear el usuario");
+  user.save( err => {
+    if (err) { return next(err) }
+    console.log("antes de redireccionar");
+    res.redirect('/private');
+  })
+});
+
+/*CRUD -> DELETE DATABASE */
+router.get('/private/delete/:id', (req, res) => {
+  const id = req.params.id;
+
+  User.findByIdAndRemove(id, (err, product) => {
+    if (err){ return next(err); }
+    return res.redirect('/private');
+  });
 });
 
 /*
