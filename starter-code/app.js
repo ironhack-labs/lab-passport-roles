2b6f544011da08576cd25c7b8f26292aab3589e9
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-02-05 20:41:57 +0100

    A few doubts left.., Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,14 +5,60 @@ const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const mongoose     = require("mongoose");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User");
+const flash = require("connect-flash");
 
 const app = express();
 
 // Controllers
 const siteController = require("./routes/siteController");
+const authController = require("./routes/auth");
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
+
+app.use(session({
+  secret: "ibi-ironhack-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +74,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use("/", siteController);
+app.use("/",authController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
