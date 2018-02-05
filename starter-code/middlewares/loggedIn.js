4897789a7d3724fadc94843b5858ab7c4b662d6e
JurgenBlitz <jorge.gopae@gmail.com>
2018-02-05 19:19:39 +0100

    Halfway through the first

---
+++ starter-code/middlewares/loggedIn.js
@@ -0,0 +1,9 @@
+
+const loggedIn = (req, res, next) => {
+    if(req.user) {
+        next();
+    } else {
+        res.redirect('/')
+    }
+}
+module.exports = loggedIn;
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,72 @@
-const express = require("express");
-const siteController = express.Router();
+const express        = require("express");
+const router         = express.Router();
+// siteController.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+// User model
+const User = require("../models/users");
+// Bcrypt to encrypt passwords
+const passport      = require("passport");
+const loggedIn = require('../middlewares/loggedIn')
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-siteController.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/signup", (req, res, next) => {
+  res.render("passport/signup");
 });
 
-module.exports = siteController;
+router.post("/passport/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("passport/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("passport/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      name,
+      familyName,
+      password: hashPass,
+      role, 
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("passport/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("passport/login");
+});
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get("/private", loggedIn, (req, res) => {
+  res.render("passport/private", { user: req.user });
+});
+
+
+module.exports = router;
