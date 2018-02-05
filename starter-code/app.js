df7ea12f5bca51a72ea3ccd2d9d4effa7f21264d
Beltran Rengifo <merloc@gmail.com>
2018-02-05 17:09:17 +0100

    initial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,18 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const expressLayouts = require('express-ejs-layouts');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const debug = require('debug')(`ibi:${path.basename(__filename).split('.')[0]}`)
+const passportConfig = require('./passport')
+const {
+  dbURL
+} = require('./config');
 
 const app = express();
 
@@ -12,32 +20,61 @@ const app = express();
 const siteController = require("./routes/siteController");
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/ibi-ironhack");
+mongoose.Promise = global.Promise;
+mongoose.connect(dbURL, {
+    useMongoClient: true
+  })
+  .then(() => debug(`Connected to ${dbURL}`))
+  .catch(e => console.log(e));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout/main-layout');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//session cookies
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+//passport config
+passportConfig(app);
+
+//locals
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.title = 'IBI';
+  next();
+})
 
 // Routes
 app.use("/", siteController);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const siteController = express.Router();
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 siteController.get("/", (req, res, next) => {
   res.render("index");
+});
+
+siteController.get('/boss', ensureAuthenticated, (req, res) => {
+  res.render('boss');
 });
 
 module.exports = siteController;
