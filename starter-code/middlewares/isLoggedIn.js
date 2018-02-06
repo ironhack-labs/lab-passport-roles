16f9cb564581b2f937ab306509db47123406bfa7
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-06 09:42:10 +0100

    Casi iteración 1

---
+++ starter-code/middlewares/isLoggedIn.js
@@ -0,0 +1,11 @@
+
+// const isLoggedIn = (req,res,next) => {
+//     if(req.user){
+//         next();
+//     }else{
+//         console.log("[Forbidden] User cannot access this page");
+//         res.redirect('/');
+//     }
+// }
+
+// module.exports = isLoggedIn;
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,74 @@
 const express = require("express");
 const siteController = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+//const ensureLogin = require("connect-ensure-login");
+const checkRoles = require("../middlewares/role")
+const isLoggedIn = require('../middlewares/isLoggedIn');
+const onlyMe = require('../middlewares/onlyMe');
 
-siteController.get("/", (req, res, next) => {
-  res.render("index");
+siteController.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+
+siteController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+siteController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+
+siteController.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+siteController.post("/login", passport.authenticate("local", {
+
+  successRedirect: "/private",
+  failureRedirect: "/auth/login"
+}));
+
+siteController.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get('/private', checkRoles('Boss'), (req, res) => {
+  res.render('private', {user: req.user});
 });
 
 module.exports = siteController;
