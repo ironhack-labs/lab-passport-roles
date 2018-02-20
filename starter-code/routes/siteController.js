68d0fd1431581ef9b1123437d1d44ed964e8abe6
AliciaJz <zilaphono@gmail.com>
2018-02-20 09:23:41 -0600

    necesito leer mas las lecciones

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const siteController = express.Router();
 
+const User           = require("../models/user");
+
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+const ensureLogin = require("connect-ensure-login");
+const passport      = require("passport");
+const flash = require("connect-flash");
+const salt = bcrypt.genSaltSync(bcryptSalt)
+
+
 siteController.get("/", (req, res, next) => {
   res.render("index");
 });
