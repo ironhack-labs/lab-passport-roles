95e804ad0d20eeed38f266d05606f017817dd8b5
Frieda Bulnes <friedabulnes@gmail.com>
2018-02-20 08:40:50 -0600

    almost done

--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,104 @@
 const express = require("express");
 const siteController = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
 
-siteController.get("/", (req, res, next) => {
-  res.render("index");
+
+//passport
+const passport = require("passport");
+//login
+siteController.get("/login", (req,res)=>{
+   res.render("sitecontroller/login");
+});
+
+siteController.post("/login", passport.authenticate("local", {
+    successRedirect: "/index",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}));
+
+// siteController.get("/index", (req,res, next)=>{
+//   res.render("sitecontroller/index");
+
+siteController.get("/signup", (req,res, next)=>{
+    res.render("sitecontroller/signup");
+})
+
+.post("/signup", (req,res,next)=>{
+    const username = req.body.username,
+          password = req.body.password;
+    if(username === "" || password === ""){
+        res.render("sitecontroller/signup", {message: "Indicate username and password"});
+        return;
+    }
+
+    User.findOne({username}, "username", (err, user)=>{
+       if (user !== null){
+           res.render("sitecontroller/signup", {message:"The username already exists"});
+           return;
+       }
+
+       const hashPass = bcrypt.hashSync(password, salt);
+
+       const newUser = new User({
+          username,
+          password:hashPass
+       });
+
+       newUser.save(err=>{
+           if (err) return res.render("sitecontroller/signup", { message: "Something went wrong" });
+            res.redirect("/");
+       });
+
+    });
 });
 
 module.exports = siteController;
+
+
+
+
+
+
+// siteController.get("/signup", (req,res, next)=>{
+//   res.render("sitecontroller/signup");
+// })
+
+// .post("/signup", (req,res,next)=>{
+//   const username = req.body.username,
+//         password = req.body.password;
+//   if(username === "" || password === ""){
+//       res.render("auth/signup", {message: "Indicate username and password"});
+//       return;
+//   }
+
+//   User.findOne({username}, "username", (err, user)=>{
+//      if (user !== null){
+//          res.render("sitecontroller/signup", {message:"The username already exists"});
+//          return;
+//      }
+
+//      const hashPass = bcrypt.hashSync(password, salt);
+
+//      const newUser = new User({
+//         username,
+//         password:hashPass
+//      });
+
+//      newUser.save(err=>{
+//          if (err) return res.render("sitecontroller/signup", { message: "Something went wrong" });
+//           res.redirect("/");
+//      });
+
+//   });
+// });
+
+// module.exports = siteController;
+
+// siteController.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+// module.exports = siteController;
