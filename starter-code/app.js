fea061aa426961c044898565f5c78a0751434a52
Ana Raballo <anitaraballo@gmail.com>
2018-02-22 17:21:48 -0500

    Inital commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,28 @@ const siteController = require("./routes/siteController");
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ibi-ironhack");
 
+// passportJS
+const session       = require("express-session");
+const bcrypt        = require("bcrypt");
+const passport      = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+// Iteration 3 - Private Page
+const ensureLogin = require("connect-ensure-login");
+const flash = require("connect-flash");
+const User = require("./models/user");
+
+
+//enable sessions
+app.use(session({
+  secret: "passport-local-strategy",
+  resave: true,
+  saveUninitialized: true
+}));
+// initialize a session
+app.use(passport.initialize());
+app.use(passport.session());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -25,9 +47,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(flash());
 // Routes
 app.use("/", siteController);
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+  },(req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/siteController.js
+++ starter-code/routes/siteController.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const siteController = express.Router();
+const passport      = require("passport");
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+const User       = require("../models/user");
 
-siteController.get("/", (req, res, next) => {
-  res.render("index");
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
+var checkBoss = checkRoles('Boss')
+var checkDeveloper = checkRoles('Developer')
+var checkTA = checkRoles('TA')
+
+siteController.get('/', (req, res, next) => {
+  res.render('index', {user: req.user});
 });
+
+siteController.get('/login', (req, res) => {
+  res.render('auth/login', {message: req.flash('error')});
+});
+
+siteController.post('/login', passport.authenticate('local', {
+  successRedirect : '/',
+  failureRedirect : '/login',
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+
+
+
+
 
 module.exports = siteController;
