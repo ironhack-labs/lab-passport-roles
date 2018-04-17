6c0a41ed965db5176f7af5b887da1b622f0bdcf3
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-17 19:41:15 +0200

    Ya lista users y formularios de edit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -23,6 +23,16 @@ authRoutes.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
+authRoutes.get("/list", (req, res, next) => {
+  
+    User.find()
+    .then(user_data => {
+      console.log(user_data)
+      res.render("auth/list", { user_data });
+    })
+    .catch(error => { console.log(error) })
+ 
+});
 
 authRoutes.post(
   "/login",
@@ -34,6 +44,67 @@ authRoutes.post(
   })
 );
 
+authRoutes.post("/bosspage", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const role = req.body.role;
+
+  if (username === "" || password === "") {
+    res.render("auth/bosspage", { message: "Indicate username and password" });
+    reject();
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) throw new Error("The username already exists");
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass,
+        role
+      });
+      return newUser.save();
+    })
+    .then(newUser => {
+      res.redirect("/auth/bosspage");
+    })
+    .catch(e => {
+      res.render("auth/signup", { message: e.message });
+    });
+});
+
+authRoutes.get('/:id/delete', (req, res) => {
+  console.log(req.params.id)
+  User.findByIdAndRemove(req.params.id)
+  .then(() => {
+    
+    res.redirect("/auth/list");
+  })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+authRoutes.get('/:id/edit', (req, res) => {
+  User.findById(req.params.id)
+  .then((user_data) => {
+
+    res.render("auth/edit", { user_data });
+  })
+  .catch(error => { console.log(error) })
+});
+
+authRoutes.post("/:id/edit", (req, res) => {
+  const { username, role } = req.body;
+  const updates = { username, role };
+
+  User.findByIdAndUpdate(req.params.id, updates)
+  .then(() => {
+    res.redirect("/auth/list");
+  });
+});
 
 
 module.exports = authRoutes;
