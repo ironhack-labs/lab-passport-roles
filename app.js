3d38d1841d1ce952f78585f1f33d5f83fd6af2bd
Javier Valiente <javara00@gmail.com>
2018-04-18 09:48:07 +0200

    :[

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const app_name = require("./package.json").name;
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
 
 
 mongoose.Promise = Promise;
@@ -19,9 +23,6 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // Middleware Setup
@@ -29,6 +30,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
+require("./passport")(app);
+
 
 // Express View engine setup
 
@@ -47,12 +61,13 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'REGISTER IN THE BEST PAGE OF THE WORLD';
 
 
 
 const index = require('./routes/index');
 app.use('/', index);
-
+const passRouter = require("./routes/passRouter");
+app.use("/pass", passRouter);
 
 module.exports = app;
---
+++ middlewares/logged.js
@@ -0,0 +1,11 @@
+const logged = (redirectTo) => (req, res, next) => {
+  if (req.user) {
+    console.log(`ACCESS GRANTED to user ${req.user.username}`);
+    next();
+  } else {
+    console.log(`ACCESS DENIED. No user, redirect!`);
+    res.redirect(redirectTo);
+  }
+};
+
+module.exports = logged;
