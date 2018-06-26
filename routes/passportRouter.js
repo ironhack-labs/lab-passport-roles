204e7b87059cc84aae62932deee922e45421153d
me@seanzamora.com <me@seanzamora.com>
2018-06-26 19:13:22 -0400

    Completed Assignment

---
+++ routes/passportRouter.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+const bcrypt = require("bcrypt");
+const bcryptSalt =10;
+const User = require('../models/user');
+
+router.get("/signup", (req, res, next) => {
+    res.render("signup");
+});
+
+router.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "" || password === "") {
+        res.render("auth/signup", {
+            message: "Username and Password Required"
+        });
+        return;
+    }
+
+    User.findOne({
+            username
+        })
+        .then(user => {
+            if (user !== null) {
+                res.render("auth/signup", {
+                    message: "Username is not available"
+                });
+                return;
+            }
+
+            const salt = bcrypt.genSaltSync(bcryptSalt);
+            const hashPass = bcrypt.hashSync(password, salt);
+
+            const newUser = new User({
+                username,
+                password: hashPass
+            });
+
+            newUser.save((err) => {
+                if (err) {
+                    res.render("auth/signup", {
+                        message: "Error, contact sire admin."
+                    });
+                } else {
+                    res.redirect("/");
+                }
+            });
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+
+
+router.get("/login", (req, res, next) => {
+    res.render("login");
+  });
+  
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/private",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}));
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/login");
+});
+
+router.get("/private", ensureLogin.ensureLoggedIn(), (req, res) => {
+res.render("private", { user: req.user });
+});
+
+module.exports = router;
