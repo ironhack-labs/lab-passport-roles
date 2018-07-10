e22ed083b3669bcf562d8b5f656257a93ea096dc
LauCF <lauracanosaf@hotmail.com>
2018-07-10 13:12:24 +0200

    Iteración 1

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const role = require("../midleware/ensureLogin");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {boss: role.isBoss()});
 });
 
 module.exports = router;
