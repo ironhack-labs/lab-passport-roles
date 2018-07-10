15546554e26e35f21ecddda428a02d9cfb59f9ee
Paula <paulagarizcun@gmail.com>
2018-07-10 13:13:09 +0200

    I1

---
+++ routes/authRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const passport = require('passport');
+const router = express.Router();
+const bcryptSalt = 10;
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+
+  const {
+    username,
+    password
+  } = req.body;
+
+  User.findOne({
+      username
+    })
+    .then(user => {
+      console.log(user);
+      if (user !== null) {
+        throw new Error("Username Already exists");
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass,
+        role: [req.body.role]
+      });
+
+      return newUser.save()
+    })
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("auth/signup", {
+        errorMessage: err.message
+      });
+    })
+})
+
+router.get('/login', (req, res, next) => {  
+  res.render('auth/login');
+});
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+)
+
+router.get('/logout' , (req,res) => {
+  req.logout();
+  res.redirect('/');
+})
+
+
+module.exports = router;
