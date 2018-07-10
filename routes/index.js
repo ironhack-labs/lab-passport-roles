fffc95b7ef1abd6f8f5b027d098bc0a5f509553f
dmarinsa <dmarinsanchez@gmail.com>
2018-07-10 13:11:42 +0200

    1st and a half iteration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hasRole = require('../middleware/checkRoles');
+const User = require('../models/User')
 const router  = express.Router();
+const ensureLogin = require('connect-ensure-login')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -13,5 +15,19 @@ router.get('/profile', [
 ] , (req,res) => {
   res.render('private/profile');
 })
+
+/* GET all celebrities */
+router.get('/celebrities', (req, res, next) => {
+  Celebritie.find({}).then( celeb => {
+    res.render('celebs/celebrities', {celeb});
+  })
+});
+
+
+router.get('/user-list', ensureLogin.ensureLoggedIn(), (req, res) => {
+  User.find({}).then( users => {
+    res.render('userList', {users});
+  })
+});
 
 module.exports = router;
