a8d109c3bd32d8e9e4de00ea6cc280d90706f9d0
diegomendezp <diegomen15@gmail.com>
2018-07-10 13:12:58 +0200

    Iteration 2

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
 
-
+const dbName = process.env.DBURL;
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/lab-passport-roles', {useMongoClient: true})
+  .connect(dbName, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -29,6 +32,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+
+require('./passport')(app);
 
 // Express View engine setup
 
@@ -44,10 +60,19 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+
+app.use((req,res,next) => {
+  // default value for title local
+  res.locals.title = 'Express - Generated with IronGenerator';
+  res.locals.user = req.user;
+  res.locals.message = req.flash("error");
+  next();
+}) 
 
 
 
