883cba0bacf4b0ba6050eab62af7e68ef913d3c9
Miguel Angel Rodriguez <finkel218@gmail.com>
2018-07-12 19:43:41 -0500

    done

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  role: {
+    type: String,
+    enum : ['Boss', 'Developer', 'T.A.'],
+    default : 'Boss'
+  }
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
