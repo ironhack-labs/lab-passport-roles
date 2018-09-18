c032e6961a42c4cdb4407cde4d41cfe6d1f62272
andrew <andrew00624@gmail.com>
2018-09-18 18:33:17 -0400

    done

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require('express-session')
+const passport = require('./helpers/passport')
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/lab-passport-roles', {useMongoClient: true})
+  .connect('mongodb://localhost/users', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -23,6 +25,18 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+
+//session
+app.use(session({
+  secret:'s3cr3t',
+  resave:true,
+  saveUninitialized:true
+}))
+
+//passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Middleware Setup
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router  = express.Router();
+const passport = require('passport')
+const User = require ('../models/User')
+
+
+function checkRole(role){
+  return (req,res,next) =>{
+    if(req.isAuthenticated() && req.user.role==role){
+      next ()
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
+//Sign up
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+router.post('/signup', (req, res, next) => {
+  User.register(req.body, req.body.password)
+  .then(user => res.redirect('/login'))
+  .catch(error => next(error))
+})
+
+
+//Login
+router.get ('/login',(req,res,next)=>{
+  res.render('login')
+})
+
+router.post('/login',passport.authenticate('local'),(req,res,next)=>{
+  res.redirect('platform')
+})
+
+
+// New
+router.get('/platform', checkRole('BOSS'), (req,res,next)=>{
+  User.find()
+  .then(users =>{
+    res.render("platform", {users})
+  })
+  
+})
+
+router.post ('/platform',(req,res,next)=>{
+  User.create(req.body)
+  .then(user =>{
+    res.redirect('/platform')
+  })
+})
+
+
+// New-form
+
+router.get('/platform/new-form', checkRole('BOSS'), (req,res,next)=>{
+  res.render("new-form")
+})
+
+router.post ('/platform/new-form',(req,res,next)=>{
+  User.create(req.body)
+  .then(user =>{
+    res.redirect('/platform')
+  })
+})
+
 
 module.exports = router;
