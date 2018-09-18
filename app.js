2357c7012b34cead100681dcd61ae850bafb52da
perlavianey <pvramosr@gmail.com>
2018-09-18 17:34:06 -0500

    PP MX JOSEPH & PERLA

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session = require('express-session')
+const passport = require('./helpers/passport')
 
 
 mongoose.Promise = Promise;
@@ -23,6 +25,17 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+//session
+app.use(session({
+  secret:'s3cr3t',
+  resave: true,
+  saveUninitialized: true
+}))
+
+//passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -53,6 +66,12 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/',auth)
+
+const ironhack = require('./routes/ironhack')
+app.use('/ironhack',ironhack)
 
 
 module.exports = app;
