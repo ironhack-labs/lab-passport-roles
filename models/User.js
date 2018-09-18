ccedce12f5efa1fbd7e89f4885cb6c2e1d0b99b8
FonRiego <fonriego@gmail.com>
2018-09-18 13:01:43 +0200

    First Iteration

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    role: {
+      type: String,
+      enum: ["BOSS", "DEVELOPER", "TA"],
+      default: "TA"
+    }
+  },
+  {
+    timestamps: {
+      creadedAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
