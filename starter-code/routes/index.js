abb3f5cda075eb11847892bda60f4d9a9ebdb6c3
Mohsan <mohsanabbas>
2018-11-12 18:36:47 -0200

    ittiration one completed

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
