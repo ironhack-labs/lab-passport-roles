e0e13297958e1ae03927c1cf646eb53c61517261
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-20 02:48:57 +0100

    Iteration 3 complete

--- starter-code/routes/courseRouter.js
+++ starter-code/routes/courseRouter.js
@@ -18,48 +18,73 @@ const ensureLogin = require("connect-ensure-login");
 
 
 function checkRoles(role) {
-  return function (req, res, next) {
-    if (req.isAuthenticated() && role.includes(req.user.role)) {
-      return next();
-    } else {
-      res.render('permissionDenied', req.user)
+    return function (req, res, next) {
+        if (req.isAuthenticated() && role.includes(req.user.role)) {
+            return next();
+        } else {
+            res.render('permissionDenied', req.user)
+        }
     }
-  }
 }
 
 courseRouter.get('/courses', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next) => {
     Course.find()
-    .then((courses)=>{
-        res.render('courses/index', {courses})
-    })
-    .catch((err)=>{
-        next(err)
-    })
+        .then((courses) => {
+            res.render('courses/index', { courses })
+        })
+        .catch((err) => {
+            next(err)
+        })
 })
 
 
-courseRouter.post('/create-course', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next)=>{
-    const {name, duration} = req.body;
-    let newCourse = new Course({name, duration});
+courseRouter.post('/create-course', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next) => {
+    const { name, duration } = req.body;
+    let newCourse = new Course({ name, duration });
 
     newCourse.save()
-    .then(()=>{
-        res.redirect('/courses');
-    })
-    .catch((err)=>{
-        next(err);
-    })
+        .then(() => {
+            res.redirect('/courses');
+        })
+        .catch((err) => {
+            next(err);
+        })
 })
 
 
 courseRouter.post('/delete-course/:id', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next) => {
     Course.findByIdAndRemove(req.params.id)
-      .then(() => {
-        res.redirect('/courses');
-      })
-      .catch((err) => {
-        next(err);
-      })
-    })
+        .then(() => {
+            res.redirect('/courses');
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+courseRouter.get('/edit-course/:id', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next) => {
+    
+    Course.findById(req.params.id)
+        .then((course) => {
+            res.render('courses/editCourse',  course );
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+courseRouter.post('/edit-course/:id', ensureLogin.ensureLoggedIn(), checkRoles(['TA']), (req, res, next) => {
+    const { name, duration } = req.body;
+    
+    Course.findByIdAndUpdate(req.params.id, { name, duration })
+        .then(() => {
+            res.redirect('/courses');
+        })
+        .catch((err) => {
+            next(err);
+            
+        })
+})
+
 
 module.exports = courseRouter;
