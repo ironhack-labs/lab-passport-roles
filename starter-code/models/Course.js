c5c3aefa17956e06c899f960ffd693e8904fb5f8
MefitHp <mefitDev@gmail.com>
2018-11-27 09:20:07 -0600

    solo 3 iteraciones y medio mal hechas

---
+++ starter-code/models/Course.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const courseSchema = new Schema({
+    name: {
+        type: String,
+        unique: true
+    }
+}, {
+    timestamps: {
+        createdAt: true,
+        updatedAt: true,
+    }
+})
+
+
+module.exports = mongoose.model('Course', courseSchema)
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require('../models/User')
+const passport = require('passport')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  User.register({username:"JEFECITO", role:"BOSS"},"password")
+  .then(user =>{
+    res.render('/')
+  }).catch(err=>{res.redirect('/login')})
 });
+
 
 module.exports = router;
