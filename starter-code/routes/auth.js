1b4587ab63d930db5f9d98278b42205daea00188
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-26 19:16:17 -0600

    add new employee

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -5,10 +5,15 @@ const User = require("../models/User");
 
 
 
-router.post('/login', passport.authenticate("local"), (req,res,next)=>{
-    res.send('entraste')
+router.post('/login', passport.authenticate("local",{failureRedirect:'/login'}), (req,res,next)=>{
+    res.redirect('/profile')
 })
 
+
+router.get('/profile', (req, res, next) => {
+  const user = req.user
+  res.render('profileBoss', user)
+});
 
 router.get('/login', (req, res, next) => {
   // User.register({username:"JEFECITO", role:"BOSS"}, 'admin')
@@ -21,6 +26,20 @@ router.get('/login', (req, res, next) => {
 
 });
 
-//res.render('/auth/login')
+
+router.get('/newEmployee', (req, res, next) => {
+  
+  res.render('newEmployee')
+
+});
+
+router.post('/newEmployee', (req, res, next) =>{
+  User.register(req.body, 'pass123')
+  .then(user=>{
+    res.send(user)
+  }).catch(err=>{
+    console.log(err)
+  })
+});
 
 module.exports = router;
