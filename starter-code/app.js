90e3872c716a89f61deed983fdb820e859fc79ab
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-26 19:27:18 -0600

    Iteration 1. Add user

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const passport     = require("./helpers/passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
 
 
 mongoose
@@ -30,6 +32,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SECRET,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}))
 
 // Express View engine setup
 
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -3,6 +3,14 @@ const router  = express.Router();
 const User = require("../models/User")
 const passport = require('passport')
 
+function checkIfIs(role){
+  return function(req, res, next){
+    if(req.user.role === role) return next()
+    return res.redirect('/profile')
+  }
+}
+
+
 /* GET home page */
 router.get('/login', (req, res, next) => {
   res.render('auth/login');
@@ -17,7 +25,29 @@ router.get('/login', (req, res, next) => {
 router.post('/login', passport.authenticate("local",{failureRedirect:"/auth/login"}), (req, res, next)=>{
   const username = req.user.username
   req.app.locals.user = req.user
-  res.send('Tu eres un usuario real con username: ' + username)
+  res.redirect('/auth/profile')
 })
 
+router.get('/profile', (req, res, next) => {
+  const user = req.user
+  res.render('profileboss', user);
+});
+
+router.get('/newemployee', (req, res, next) => {
+  res.render('newemployee');
+});
+
+router.post('/newemployee', (req, res, next) => {
+  User.register(req.body,"pass1234")
+  .then(user=>{
+    res.json(user)
+  }).catch(e => next(e))
+});
+
+router.get('/list', checkIfIs("BOSS") (req, res, next) => {
+  User.find()
+  .then(users=>{
+    res.render('list',{users})
+  }).catch(e => next(e))
+});
 module.exports = router;
