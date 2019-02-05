6d2c5859c5239ccbb00f07c9de36a953a033bdfc
Sofía <sofianombela@gmail.com>
2019-02-05 09:29:30 +0100

    try to correct errors

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -14,49 +14,10 @@ const LocalStrategy = require("passport-local").Strategy;
 const ensureLogin = require("connect-ensure-login");
 const flash = require("connect-flash");
 
-app.use(
-  session({
-    secret: "our-passport-local-strategy-app",
-    resave: true,
-    saveUninitialized: true
-  })
-);
 
-passport.serializeUser((user, cb) => {
-  cb(null, user._id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
-});
-
-passport.use(
-  new LocalStrategy((username, password, next) => {
-    User.findOne({ username }, (err, user) => {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return next(null, false, { message: "Incorrect username" });
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return next(null, false, { message: "Incorrect password" });
-      }
-
-      return next(null, user);
-    });
-  })
-);
 
 //midlleware
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+
 
 router.get("/login", (req, res, next) => {
   res.render("login");
@@ -65,7 +26,7 @@ router.get("/login", (req, res, next) => {
 router.post(
   "/login",
   passport.authenticate("local", {
-    successRedirect: "/success",
+    successRedirect: "/private-page",
     failureRedirect: "/fail",
     failureFlash: true,
     passReqToCallback: true
