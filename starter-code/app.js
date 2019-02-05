7c688ed8f4fcd543963a8721ff277487ed8edd94
Thilo Sperber <t.sperber@gmx.net>
2019-02-05 09:20:14 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,9 +9,15 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/passport-roles', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,6 +35,39 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 // Express View engine setup
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,106 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const flash = require("connect-flash");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+
+const checkBoss = checkRoles("BOSS");
+const checkDeveloper = checkRoles("DEVELOPER");
+const checkTA = checkRoles("TA");
+const session = require("express-session");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+//LOGIN
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successReturnToOrRedirect: "/administration",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+// ADMINISTRATION - BOSS
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  };
+}
+router.get("/administration", checkBoss, (req, res) => {
+  res.render("administration", { user});
+});
+
+router.post("/administration", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const role = req.body.role;
+
+  if (username === "" || password === "") {
+    res.render("base", {
+      message: "Indicate username and password",
+      section: "administration"
+    });
+    return;
+  }
+
+  User.findOne({
+    username
+  })
+    .then(user => {
+      if (user !== null) {
+        res.render("base", {
+          message: "The username already exists",
+          section: "administration"
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass,
+        role
+      });
+
+      newUser.save(err => {
+        if (err) {
+          res.render("base", {
+            message: "Something went wrong",
+            section: "administration"
+          });
+        } else {
+          res.redirect("/");
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+//LOG OUT
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(err => {
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
