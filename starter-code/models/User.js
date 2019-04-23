8230f5446a646612f74b61db1992eaca4b97b674
brennedith <brennedithg@gmail.com>
2019-04-22 23:32:34 -0500

    1st and 2nd Iterations

---
+++ starter-code/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    name: String,
+    bio: String,
+    role: {
+      type: String,
+      required: true,
+      enum: ['Boss', 'TA', 'Developer', 'Alumni']
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
