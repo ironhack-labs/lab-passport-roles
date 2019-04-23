291b266146d6e4ed27c2443230c166fd49ef79ef
pipepico <fpr29.10@gmail.com>
2019-04-23 00:57:25 -0500

    init

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const passport     = require('./helpers/passport')
+const session      = require('express-session')
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
