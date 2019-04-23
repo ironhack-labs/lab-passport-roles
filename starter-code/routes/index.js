65c44f8f137b847bec056fdfef90c13b3ae28a3a
deredhuzent <hakurascot@gmail.com>
2019-04-23 00:06:42 -0500

    starting wRoutes

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express').Router();
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
 
 module.exports = router;
