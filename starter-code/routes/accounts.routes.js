1d8e44e71910017e71bda8f51b670ce3400f9d13
rogerayra <roger.ayra@gmail.com>
2019-07-02 03:07:49 -0500

    Solucionado un problema en que no se aseguraba tener sessión activa en ciertas rutas

--- starter-code/routes/accounts.routes.js
+++ starter-code/routes/accounts.routes.js
@@ -1,16 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const checkRole = require('../middlewares/checkRole')
+const checkRole = require("../middlewares/checkRole");
+const { ensureLoggedIn } = require("connect-ensure-login");
 const { getCreate, postCreate, getAll, getAccount, postAccount, getDelete } = require("../controllers/accounts.controllers");
 
 router.get("/create", checkRole("BOSS"), getCreate);
 router.post("/create", checkRole("BOSS"), postCreate);
 
-router.get("/all", getAll);
+router.get("/all", ensureLoggedIn({ redirectTo: "/auth/login" }), getAll);
 
 router.get("/:id/delete", checkRole("BOSS", true), getDelete);
 
-router.get("/:id", getAccount);
+router.get("/:id", ensureLoggedIn({ redirectTo: "/auth/login" }), getAccount);
 router.post("/:id", checkRole("BOSS", true), postAccount);
 
 module.exports = router;
