4dcba8ad0e5ddb5fa81318bde4c37770c44a9c33
Gabriel B <gabelmo92@hotmail.com>
2019-07-02 09:14:02 +0200

    done2

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -1,15 +1,19 @@
+// models/user.js
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  role: {
-    type: String,
-    enum : ['BOSS', 'DEVELOPER', 'TA'],
-    default : 'TA'
-  }
+    username: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ['BOSS', 'DEVELOPER', 'TA'],
+        default: 'TA'
+    }
 }, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
+
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
