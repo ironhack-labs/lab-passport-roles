775f5c362cfe8ab585dbb9d914bc82baf62fffba
Iván López Ruiz <ivan@MacBook-Air-de-Ivan.local>
2019-07-02 09:29:27 +0200

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/user');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
 
 module.exports = router;
