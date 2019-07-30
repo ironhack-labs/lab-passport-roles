fcbf1291b900ff199f666b3aaacdd25d769be79a
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-30 09:13:47 +0200

    iteracion 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,22 +8,34 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session   = require('express-session');
+const flash = require("connect-flash")
+const passport = require("passport");
 
+
+require('./configs/passport.config.js')
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+.connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+.then(x => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err)
+});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
-
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -54,5 +66,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+const pass = require('./routes/passport.routes');
+app.use('/', pass);
 
 module.exports = app;
