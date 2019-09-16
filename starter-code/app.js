eb03a4fce148a16ccd2490e6542609746cfbbfca
Pepe Sainz <psainz2593@gmail.com>
2019-09-16 17:50:15 +0200

    falta

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,22 @@
+
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
 
 
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+require('./configs/db.config');
+require('./configs/passport.config');
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -33,21 +32,28 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
+app.use(session({
+  secret: 'IBI',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = '🤟🏾';
 
 
 
---
+++ starter-code/models/User.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['BOSS', 'DEVELOPER', 'TA', 'ALUMNI'],
+  },
+  // timestamps: true,  
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
