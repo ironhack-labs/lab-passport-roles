6ff371920b3b2bf4acab5f71bc8e6a70f36f6647
“Natalia <natgarea@gmail.com>
2019-09-16 22:47:19 +0200

    second iteration - employees

--- starter-code/routes/passport.routes.js
+++ starter-code/routes/passport.routes.js
@@ -2,66 +2,117 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const User = require("../models/User");
-const secure = require('../middlewares/secure.mid');
-
+const secure = require("../middlewares/secure.mid");
 
 const passportRouter = express.Router();
 const bcryptSalt = 10;
 
-passportRouter.get('/', (req, res, next) => {
-  res.render('auth/login');
+// Log in
+passportRouter.get("/", (req, res, next) => {
+  res.render("passport/login");
 });
 
-passportRouter.post('/', passport.authenticate('local-auth', {
-  successRedirect: '/admin',
-  failureRedirect: '/',
-  passReqToCallback: true,
-  failureFlash: true,
-}));
+passportRouter.post(
+  "/",
+  passport.authenticate("local-auth", {
+    successRedirect: "/private",
+    failureRedirect: "/",
+    passReqToCallback: true,
+    failureFlash: true
+  })
+);
 
+// Creating users
 passportRouter.get("/create-employee", (req, res, next) => {
-  res.render("auth/signup");
+  res.render("passport/signup");
 });
 
-passportRouter.post('/create-employee', secure.checkRole('Boss'), (req, res, next) => {
-  const { username, password, role } = req.body;
+passportRouter.post(
+  "/create-employee",
+  secure.checkRole("Boss"),
+  (req, res, next) => {
+    const { username, password, role } = req.body;
 
-  if (username === '' || password === '') {
-    res.render('auth/signup', { message: 'Please indicate username and password' });
-  }
-
-  User.findOne({ username })
-    .then((user) => {
-      if (user) {
-        res.render('auth/signup', { message: 'Username already exists' });
-      }
-
-      const salt = bcrypt.genSaltSync(bcryptSalt);
-      const hashPass = bcrypt.hashSync(password, salt);
-
-      const newUser = new User({
-        username,
-        password: hashPass,
-        role
+    if (username === "" || password === "") {
+      res.render("passport/signup", {
+        message: "Please indicate username and password"
       });
+    }
 
-      newUser.save()
-        .then(() => res.redirect('/'))
-        .catch(error => next(error));
+    User.findOne({ username })
+      .then(user => {
+        if (user) {
+          res.render("passport/signup", { message: "Username already exists" });
+        }
+
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        const newUser = new User({
+          username,
+          password: hashPass,
+          role
+        });
+
+        newUser
+          .save()
+          .then(() => res.redirect("/users"))
+          .catch(error => next(error));
+      })
+      .catch(error => next(error));
+  }
+);
+
+// Private area
+passportRouter.get("/private", secure.checkLogin, (req, res) => {
+  res.render("passport/private", {
+    user: req.user,
+    isBoss: req.user.role === "Boss"
+  });
+});
+
+// List of employees
+passportRouter.get("/users", secure.checkLogin, (req, res, next) => {
+  User.find()
+    .then(users => {
+      res.render("passport/users", { users, isBoss: req.user.role === "Boss" });
     })
-    .catch(error => next(error));
+    .catch(err => next(err));
 });
 
-passportRouter.get('/admin', secure.checkRole('Boss'), (req, res, next) => {
-  res.render('auth/admin', { user: req.user });
+// Employee detail
+passportRouter.get("/users/:id", secure.checkLogin, (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => {
+      res.render("passport/user", { user, isSameUser: (req.params.id === req.user.id) });
+    })
+    .catch(err => next(err));
 });
 
+passportRouter.post(
+  "/users/:id",
+  (req, res, next) => {
+    if (req.params.id === req.user.id) {
+      User.updateOne(
+        { _id: req.params.id },
+        { $set: { username: req.body.username } }
+      ).then(user => {
+        res.redirect("/users/" + req.params.id);
+      }).catch(err => next(err));
+    } else {
+      res.redirect("/users");
+    }
+  }
+);
 
-passportRouter.get(
-  "/private",
-  secure.checkLogin,
-  (req, res) => {
-    res.render("auth/private", { user: req.user });
+// Delete employees
+passportRouter.post(
+  "/users/:id/delete",
+  secure.checkRole("Boss"),
+  (req, res, next) => {
+    User.findByIdAndRemove(req.params.id)
+      .then(res.redirect("/users"))
+      .catch(err => next(err));
   }
 );
 
