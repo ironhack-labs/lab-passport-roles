92839acdcbedd5cf7372e0f83a0296fee05a6497
angelmc32 <angel.melendez.c@gmail.com>
2019-10-02 06:35:59 -0500

    Add router and started views

--- lab-passport-roles/routes/index.js
+++ lab-passport-roles/routes/index.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/User');
+
+const isAuth = (req, res, next) => {
+
+  if ( req.isAuthenticated() ) return next();
+
+  let error = 'Please log in';
+  return res.render('passport/login', { error });
+
+}
+
+const checkRole = roles => {
+
+  return (req, res, next) => {
+
+    if ( roles.includes(req.user.role) ) return next();
+
+    let error = 'You are not authorized to access this page';
+    return res.render('/', { error });
+
+  }
+
+}
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', isAuth, (req, res, next) => {
+
+  const { user } = req;
+  res.render('index', { user });
+
+});
+
+router.get('/users', isAuth, checkRole(['Boss', 'Developer', 'TA']), (req, res, next) => {
+
+  User.find()
+  .then( users => {
+
+    res.render('users/index', { users });
+
+  })
+  .catch( error => console.log(error) );
+
+});
+
+router.post('/users', isAuth, checkRole('Boss'), (req, res, next) => {
+
+  const { email, password, displayName, role } = req.body;
+  let error;
+
+  if ( password !== req.body['confirm-password'] ) {
+    error = 'Make sure to enter the same password';
+    return res.render('users/new', { error });
+  }
+
+  if ( !email || !password ) {
+    error = 'Please enter email or password';
+    return res.render('users/new', { error });
+  }
+
+  User.register({ email, displayName, role }, password)
+  .then( user => {
+    req.login(user, err => {
+      res.redirect('/users');
+    });
+  })
+  .catch( error => res.render('users/new', { error }));
+
+})
+
+router.get('/users/new', isAuth, checkRole('Boss'), (req, res, next) => {
+
+  res.render('users/new');
+
 });
 
 module.exports = router;
--- lab-passport-roles/routes/passportRouter.js
+++ lab-passport-roles/routes/passportRouter.js
@@ -7,5 +7,59 @@ passportRouter.get('/login', (req, res, next) => {
   res.render('passport/login');
 });
 
+passportRouter.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info = {}) => {
+
+    const { message: error } = info;
+
+    if ( error ) return res.render('passport/login', { error });
+    req.login(user, err => res.redirect('/') );
+    console.log(err, user, info);
+  })(req, res);
+
+});
+
+passportRouter.get('/facebook', passport.authenticate('facebook'));
+
+passportRouter.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+passportRouter.get('/signup', (req, res, next) => {
+  res.render('passport/signup');
+});
+
+passportRouter.post('/signup', (req, res, next) => {
+  
+  const { email, password } = req.body;
+  let error;
+
+  if ( password !== req.body['confirm-password'] ) {
+    error = 'Make sure to enter the same password';
+    return res.render('passport/signup', { error });
+  }
+
+  if ( !email || !password ) {
+    error = 'Please enter email or password';
+    return res.render('passport/signup', { error });
+  }
+
+  User.register({ email }, password)
+  .then( user => {
+
+    req.login( user, err => res.redirect('/'));
+
+  })
+  .catch( error => res.render('passport/signup', { error }));
+
+});
+
+passportRouter.get('/logout', (req, res, next) => {
+
+  req.logout();
+  res.redirect('/login');
+
+})
 
 module.exports = passportRouter;
