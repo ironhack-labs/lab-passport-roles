c5f27d260181dc1ca7c1bc7956eaff5f48b15f17
Millene <millenems06@gmail.com>
2019-10-02 09:09:22 -0300

    not finished

---
+++ starter-code/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const UserSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  role: {
+    type: String,
+    enum : ['GUEST', 'EDITOR', 'ADMIN'],
+    default : 'GUEST'
+  }, 
+});
+
+const bossUser = mongoose.Schema({
+  
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router  = express.Router();
+const checkGuest  = checkRoles('GUEST');
+const checkEditor = checkRoles('EDITOR');
+const checkAdmin  = checkRoles('ADMIN');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/private', checkRoles('ADMIN'), (req, res) => {
+  res.render('private', {user: req.user});
+});
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
+
+router.get('/private', checkAdmin, (req, res) => {
+  res.render('private', {user: req.user});
+});
+
+router.get('/', checkEditor, (req, res) => {
+  res.render('', {user: req.user});
+});
+
+router.get('/', checkGuest, (req, res) => {
+  res.render('', {user: req.user});
 });
 
 module.exports = router;
