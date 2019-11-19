5e4133e904a383c797d259ac1ef93b0ceb3aa79c
Sonia Cáceres Benítez <soniacaceresbenitez@MacBook-Air-de-Sonia.local>
2019-11-19 09:37:53 +0100

    corregido

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,17 +1,14 @@
-const mongoose = require('mongoose');
-const Users = require('../models/User')
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose')
+const Users = require('../models/User');
+const bcrypt = require("bcrypt")
 const bcryptSalt = 12;
+const databaseIH = 'ironhack';
 
+const salt = bcrypt.genSaltSync(bcryptSalt);
+const hashPass = bcrypt.hashSync('1234', salt);
 
-mongoose
-    .connect('mongodb://localhost/ironhack', { useNewUrlParser: true })
-    .then(x => {
-        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-    })
-    .catch(err => {
-        console.error('Error connecting to mongo', err)
-    });
+mongoose.connect(`mongodb://localhost/${databaseIH}'`, { useNewUrlParser: true })
+
 
 const users = [
     {
@@ -31,22 +28,10 @@ const users = [
     }
 ]
 
-users.map(user => {
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(user.password, salt);
-
-    user.password = hashPass;
-})
-
-Users
-    .deleteMany()
-    .then(() => {
-        Users
-            .insertMany(users)
-            .then(databaseIH => {
-                console.log({ alert: 'Database ok' }, databaseIH)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
+Users.create(initialUser, (err) => {
+    if (err) {
+        throw err
+    }
+    console.log(`Created ${users.length} users`)
+    mongoose.connection.close;
+});
