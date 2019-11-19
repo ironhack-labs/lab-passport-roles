637122ad775382337b267232bedec01da06ec21a
adelcanto <a.delcanto@outlook.com>
2019-11-19 09:30:42 +0100

    Encrypted seeds

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose')
 const Users = require('../models/user');
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10;
+
 
 const dbName = 'IBI';
 mongoose.connect(`mongodb://localhost/${dbName}`);
 
 
+const salt = bcrypt.genSaltSync(bcryptSalt);
+const hashPass = bcrypt.hashSync('123', salt);
+
 const initialUser = [{
     username: 'Alfredo del Canto',
-    password: '123',
+    password: hashPass,
     role: "BOSS"
   }    
 ]
