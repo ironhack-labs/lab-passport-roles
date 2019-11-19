e40aea5fa2f47c6e4b4890ca13140fe02d52cb48
Borja Valdés Jiménez <borja@MacBook-Air-de-Borja.local>
2019-11-19 09:32:58 +0100

    ......

---
+++ starter-code/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+       type:String,
+       required: true,
+       unique:true
+    },
+    password:{
+        type: String,
+        required: true
+},
+role :{
+    type:String,
+    enum:[
+        "Boss",
+        "Developer",
+        "TA"
+    ],
+    default:"TA"
+  } 
+})
+    
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User
