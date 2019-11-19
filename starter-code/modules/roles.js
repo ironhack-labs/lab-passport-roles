9ce8ec9bab60aaf61115cd2f7c5139b5c2d32fe2
gabzafra <gab.zafra@gmail.com>
2019-11-19 09:09:45 +0100

    tidy up

--- starter-code/modules/roles.js
+++ starter-code/modules/roles.js
@@ -1,11 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const bcrypt = require("bcrypt");
-// const bcryptSalt = 10;
- const passport = require("passport");
-// const ensureLogin = require("connect-ensure-login");
-// const User = require("../models/user");
-
 function checkRoles(roles) {
   return function (req, res, next) {
     if (req.isAuthenticated() && roles.includes(req.user.role)) {
