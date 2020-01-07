ec2216a34c6ef7a8fd18ef2db9c741e137f2e92c
henricasi <henriquecmandruzato@gmail.com>
2020-01-07 18:27:18 -0300

    Iteration 1 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,17 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+// my packages and other 
+const User = require('./models/user');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const flash = require("connect-flash");
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/lab-passport-roles', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -44,15 +51,79 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+// session middleware
+app.use(session({
+  secret: 'abobrinha',
+  cookie: {
+    maxAge: 60000
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: true,
+  saveUninitialized: true
+}));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Lab - Passport Roles';
 
+// PASSPORT CONFIG
+// cookie
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+// session
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+// initialising passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// adding error handling flash
+app.use(flash());
+
+// passport local strategy config
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, {
+        message: "Incorrect username"
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {
+        message: "Incorrect password"
+      });
+    }
+
+    // success - user is logged in!
+    app.locals.user = user;
+    return next(null, user);
+  });
+}));
 
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/', auth);
 
 
 module.exports = app;
---
+++ starter-code/routes/auth.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router  = express.Router();
+// User model
+const User = require("../models/user");
+// BCrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+// adding passport for auth routes
+const passport = require("passport");
+const {checkRoles, checkBoss} = require('../middlewares/roles')
+
+router.get("/signup", checkBoss, (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const {username, password, role} = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({
+      username
+    })
+    .then(user => {
+      if (user) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists!"
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      User.create({
+          username,
+          password: hashPass,
+          role
+        })
+        .then(() => {
+          res.redirect("/");
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    })
+    .catch(error => {
+      next(error);
+    })
+
+});
+
+// LOGIN
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", {
+    message: req.flash("error")
+  });
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+module.exports = router;
