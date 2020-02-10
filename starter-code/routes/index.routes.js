998942c00b8459e486d5cea5ff6cab3d2c1297d4
Roberto Marrero <robertomarrerop@hotmail.com>
2020-02-10 19:18:51 +0100

    me falta que salga la lista completa de usuarios (y sigue)

--- starter-code/routes/index.routes.js
+++ starter-code/routes/index.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.model')
 
 // Renderizar contenido despendiendo del rol, en la vista
 const isAdmin = user => user && user.role === 'BOSS'
@@ -9,6 +10,12 @@ const checkLoggedIn = (req, res, next) => req.user ? next() : res.render('index'
 
 router.get('/', (req, res) => res.render('index', { isAdmin: isAdmin(req.user) }))
 
-router.get("/profile", checkLoggedIn, (req, res) => res.render("profile", { user: req.user }));
+router.get('/profile', checkLoggedIn, (req, res) => res.render('profile', { user: req.user }));
+
+router.get('/user-list', (req, res) => {
+  User.find()
+    .then(allUsers => res.render('/user-list', { users: allUsers }))
+    .catch(err => console.log('Error consultando la BBDD: ', err))
+})
 
 module.exports = router
