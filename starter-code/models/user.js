1acf9b0275cfac2fdc5d2dc87d0fac40c93fa88f
armetegithub <armomomete@protonmail.com>
2020-02-11 01:56:55 +0100

    Añadido el selected

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     username: String,
-    password: String
+    password: String,
+    role: { type: String, enum: ["BOSS", "DEVELOPER", "TA"] }
   },
   {
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
