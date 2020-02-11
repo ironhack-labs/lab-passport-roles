93e863d052442b165dcde2124a5e0a13aaf5a59b
Roberto Marrero <robertomarrerop@hotmail.com>
2020-02-11 09:07:29 +0100

    intentando añadir el listado de usuarios

--- starter-code/routes/index.routes.js
+++ starter-code/routes/index.routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => res.render('index', { isAdmin: isAdmin(req.user) }
 
 router.get('/profile', checkLoggedIn, (req, res) => res.render('profile', { user: req.user }));
 
-router.get('/user-list', (req, res) => {
+router.get('/userList', (req, res) => {
   User.find()
     .then(allUsers => res.render('/user-list', { users: allUsers }))
     .catch(err => console.log('Error consultando la BBDD: ', err))
