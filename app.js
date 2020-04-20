73c0dc034560d5d7733dcc59718d2df56548c3c8
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-20 18:00:35 +0200

    add passport middleware

--- app.js
+++ app.js
@@ -1,47 +1,98 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const favicon = require('serve-favicon')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const path = require('path')
+
+const session = require('express-session')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/User.model')
 
 mongoose
   .connect('mongodb://localhost/passport-roles', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error('Error connecting to mongo', err))
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require('./package.json').name
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+)
 
-const app = express();
+const app = express()
 
 // Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err)
+    }
+    cb(null, user)
+  })
+})
+
+passport.use(
+  new LocalStrategy((name, password, next) => {
+    User.findOne({ name }, (err, user) => {
+      if (err) {
+        return next(err)
+      }
+      if (!user) {
+        return next(null, false, { errorMsg: 'Incorrect username' })
+      }
+      if (!bcrypt.compareSync(password.user.password)) {
+        return next(null, false, { errorMsg: 'Incorrect password' })
+      }
+      return next(null, user)
+    })
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Express View engine setup
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // default value for title local
-app.locals.title = 'Ironhack Bureau of Investigation';
+app.locals.title = 'Ironhack Bureau of Investigation'
 
-const index = require('./routes/index.routes');
-app.use('/', index);
-const authRoutes = require('./routes/auth.routes');
-app.use('/', authRoutes);
+const index = require('./routes/index.routes')
+app.use('/', index)
+const authRoutes = require('./routes/auth.routes')
+app.use('/', authRoutes)
 
-module.exports = app;
+module.exports = app
