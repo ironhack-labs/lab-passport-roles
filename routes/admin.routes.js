ed9fe421bb9ea6a490a0d0e3a2585095d97acbd2
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-04-20 23:42:10 +0200

    boss can remove and add employees

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -10,8 +10,23 @@ router.get('/', checkRole('BOSS'), (req, res, next) => {
     .catch(err => console.log(`An error ocurred: ${err}`)) 
 })
 
-//delete
-
 //add
+router.get('/new', (req, res, next) => res.render('admin/add'))
+router.post('/', (req, res, next) => {
+    const { name, username, role, password } = req.body
+
+    User.create({ name, username, role, password })
+    .then(() => res.redirect('/admin'))
+    .catch(err => console.log(`An error ocurred adding the user: ${err}`))
+})
+
+//delete
+router.post('/:id/delete', (req, res, next) => {
+    const userId = req.params.id
+    User.findByIdAndRemove(userId)
+    .then(()  => res.redirect('/admin'))
+    .catch(err => console.log(`An error ocurred deleting the user: ${err}`))
+})
+
 
 module.exports = router
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,11 +3,6 @@ const router = express.Router();
 
 const ensureLoggedIn = (req, res, next) => req.isAuthenticated() ? next(): res.redirect('/login')
 
-// const checkRole = role => (req, res, next) => req.isAuthenticated() && req.user.role.includes(role) ? next() : res.render('auth/login', { errorMsg: 'Acceso solo para Admin' })
-// router.get('/admin', checkRole('BOSS'), (req, res) => res.render('administrador/admin'))
-
-
-
 /* GET home page */
 router.get('/', (req, res) => res.render('index'));
 router.get('/profile', ensureLoggedIn, (req, res) => res.render('auth/profile', req.user))
