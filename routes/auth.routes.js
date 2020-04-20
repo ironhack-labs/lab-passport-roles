146d2dc121407aaba323f5241f0f2eaf009beeb6
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-20 17:33:13 +0200

    create PepePiscinas user - to be declared BOSS through Compass

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,4 +14,37 @@ const passport = require('passport')
 
 router.get('/signup', (req, res) => res.render('auth/signup'))
 
+router.post('/signup', (req, res, next) => {
+  const { name, password } = req.body
+
+  if (!name || !password) {
+    res.render('auth/signup', {
+      errorMsg: 'Please provide an username and password.',
+    })
+    return
+  }
+
+  User.findOne({ name })
+    .then((user) => {
+      if (user) {
+        res.render('auth/signup', { errorMsg: 'This username already exists.' })
+        return
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
+
+      User.create({ name, password: hashPass })
+        .then(() => {
+          res.redirect('/')
+        })
+        .catch(() => {
+          res.render('auth/signup', {
+            errorMsg: 'Could not create user, please try again.',
+          })
+        })
+    })
+    .catch((err) => next(err))
+})
+
 module.exports = router
