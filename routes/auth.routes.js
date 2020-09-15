87f4c7bea12196e0b59933c8d9d8e6b23404b9c4
aretius <jonarechalde@gmail.com>
2020-09-15 08:43:42 +0200

    Endpoints cleaning

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,42 +8,6 @@ const bcrypt = require("bcrypt")
 const bcryptSalt = 10
 
 // Endpoints
-router.get('/register', (req, res) => res.render('auth/sign-up'))
-
-router.post('/register', (req, res, next) => {
-
-    const { username, password } = req.body
-
-    // If some field is empty
-    if (!username.length || !password.length) {
-
-        res.render('auth/sign-up', { message: 'Rellene todos los campos por favor' })
-
-        return
-    }
-
-    // We verify that this username is not already in use
-    User.findOne({ username })
-        .then(matchedUser => {
-
-            if (matchedUser) { // Si ya existe un usuario con este username, expulsamos
-                res.render('auth/sign-up', { message: 'Usuario ya registrado' })
-                return
-            }
-
-            // Encryptamos la contraseña
-            const salt = bcrypt.genSaltSync(bcryptSalt)
-            const hashPass = bcrypt.hashSync(password, salt)
-
-            // We create the user and redirect him to the employees index
-            User.create({ username, password: hashPass })
-                .then((newUser) => res.redirect(`/employees/profile/${newUser.id}`))
-                .catch(err => console.log(err))
-
-        })
-        .catch(error => console.log('ERROR: ', error))
-
-})
 
 router.get("/login", (req, res, next) => {
 
