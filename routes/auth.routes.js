071092e129d87c688efbb0724474b6661160404d
manuelbarreda <manubarreda69@gmail.com>
2020-11-16 19:28:55 +0100

    Done Iteration 1 and 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,66 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const passport = require("passport")
 
-// add routes here
+const User = require("../models/user.model")
 
-module.exports = router;
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10
+
+
+
+// Registro (renderizado formualrio)
+router.get("/registro", (req, res) => res.render("auth/signup"))
+
+// Registro (gestión)
+router.post("/registro", (req, res, next) => {
+
+    const { username, password } = req.body
+
+    if (username === "" || password === "") {
+        res.render("auth/signup", { errorMsg: "Rellena todos los campos" })
+        return
+    }
+
+    User
+        .findOne({ username })
+        .then(user => {
+            if (user) {
+                res.render("auth/signup", { errorMsg: "El usuario ya existe" })
+                return
+            }
+
+            // Other validations
+            const salt = bcrypt.genSaltSync(bcryptSalt)
+            const hashPass = bcrypt.hashSync(password, salt)
+
+            User.create({ username, password: hashPass })
+                .then(() => res.redirect('/'))
+                .catch(() => res.render("auth/signup", { errorMsg: "Hubo un error" }))
+        })
+        .catch(error => next(error))
+})
+
+
+
+
+// Inicio sesión (renderizado formulario)
+router.get("/inicio-sesion", (req, res) => res.render("auth/login", { errorMsg: req.flash("error") }))
+
+// Inicio sesión (gestión)
+router.post("/inicio-sesion", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/inicio-sesion",
+    failureFlash: true,
+    passReqToCallback: true
+}))
+
+
+// Cerrar sesión
+router.get('/cerrar-sesion', (req, res) => {
+    req.logout()
+    res.redirect("/inicio-sesion")
+})
+
+
+module.exports = router
