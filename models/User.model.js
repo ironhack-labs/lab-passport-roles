7ad6a6fc4c779202558caac6a4330f74d308d893
ta-web-mad <ta-web-mad@ironhack.com>
2022-02-08 11:40:54 +0100

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -5,8 +5,8 @@ const userSchema = new Schema(
     username: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: String,
-    profileImg: { type: String, required: true, default: 'https://i.stack.imgur.com/l60Hf.png' },
-    description: { type: String, required: true, default: 'No existe descripción.' }
+    profileImg: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' },
+    description: { type: String, default: 'No existe descripción.' }
     // add roles setup here
   },
   {
